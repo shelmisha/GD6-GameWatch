802c688047f4f6a63367dbf97fc06e3321136fb2
Ariana Morgana <emorgana.1995@outlook.com>
2023-01-28 20:00:57 +0100

    added player movement, layer adjustments

--- GD6-Game&Watch/Assets/Player_Movement.cs
+++ GD6-Game&Watch/Assets/Player_Movement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,28 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");  
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    
+    private void FixedUpdate() 
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    
+    }
+
+   private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
     }
 }
